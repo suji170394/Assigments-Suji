adfc22fb69439a04a1916927a101f00daaaf3f23
suji170394 <capesuji@gmail.com>
2026-04-25 19:56:51 -0500

    Add files via upload

---
+++ week1/day1/chrome.java
@@ -0,0 +1,23 @@
+package org.testleaf.week1.day1;
+
+public class chrome {
+
+    public static void main(String[] args) {
+        
+        float version = 91.0f;
+        System.out.println(version);
+
+        String developer = "Google";
+        System.out.println(developer);
+
+        boolean beta = false;
+        System.out.println(beta);
+
+        int releaseYear = 2008;
+        System.out.println(releaseYear);
+
+        char shortcutKey = 'c';
+        System.out.println(shortcutKey);
+
+    }
+}
---
+++ week1/day1/learningjava.java
@@ -0,0 +1,8 @@
+package org.testleaf.week1.day1;
+
+public class learningjava {
+
+    public static void main(String args[]){
+        System.out.println("Welcome testleaf");
+    }
+}
