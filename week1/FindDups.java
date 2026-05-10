eec89468b3a715f9e88edd6d9a2d6aec8a2569ae
suji170394 <capesuji@gmail.com>
2026-05-09 22:26:11 -0500

    Add files via upload

---
+++ week1/FindDups.java
@@ -0,0 +1,26 @@
+package day3;
+import java.util.Arrays;
+
+public class FindDups 
+{
+    public static void main(String[] args) 
+    {
+        
+        int[] num={2,5,7,7,5,9,2,3};
+        Arrays.sort(num);
+        System.out.println("Duplicate values");
+
+            for (int i = 0; i<num.length; i++) 
+                {
+            
+               if(num[i] == num[i+1]) 
+            
+{
+                    System.out.println(num[i]);
+
+}
+
+}
+
+}
+}
